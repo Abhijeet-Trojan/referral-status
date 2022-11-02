e199eed40ac677e12694b2cb2a2cb5bb300ba132
Abhijeet-Trojan <trojanabhijeet@gmail.com>
2022-11-02 16:37:31 +0530

    Updates

---
+++ precache-manifest.fc728079b62e0f9a34840a7ff3a5e601.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "07bd7a02b7b1edb6f185",
+    "url": "/referral-status/static/css/main.266d1dd0.chunk.css"
+  },
+  {
+    "revision": "07bd7a02b7b1edb6f185",
+    "url": "/referral-status/static/js/main.07bd7a02.chunk.js"
+  },
+  {
+    "revision": "84222fa2bcc59244feb4",
+    "url": "/referral-status/static/css/1.e1cc9c6e.chunk.css"
+  },
+  {
+    "revision": "84222fa2bcc59244feb4",
+    "url": "/referral-status/static/js/1.84222fa2.chunk.js"
+  },
+  {
+    "revision": "e98c140715a726d726c8",
+    "url": "/referral-status/static/js/runtime~main.e98c1407.js"
+  },
+  {
+    "revision": "df346772b0c54d300da108e5f7e360ae",
+    "url": "/referral-status/index.html"
+  }
+];
